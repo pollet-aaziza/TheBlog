53f2ed133ea3d033f35844babed3cb3dbcfe45c5
aziza <aaziza.pollet@gmail.com>
2019-04-09 05:03:20 +0200

    controller to delete are coded

--- controller/commentsController.php
+++ controller/commentsController.php
@@ -1,11 +1,21 @@
 <?php
 class commentsController
 {
+    //get comments
     function listComments()
     {
         $commentsManager = new commentsManager();
         $comments =$commentsManager->getAllComments();
         require "views/comments.php";
     }
+    //delete comment
+  public function deleteComment(){
+    if(isset($_GET["id"])) {
+      $id = htmlspecialchars($_GET("id"));
+      $commentsManager = new commentsManager();
+      if($commentsManager->deletePost($id))
+      redirectTo("");
+    }
+  }
 }
 ?>
